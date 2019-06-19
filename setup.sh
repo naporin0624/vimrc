d7097a296ab896d72be4dd92a324d3042ce457b0
naporin0624 <naporin0624@gmail.com>
2019-06-19 17:27:38 +0900

    a

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/altercation/vim-colors-solarized.git
+cd vim-colors-solarized/colors
+
+mkdir ~/.vim/
+mkdir ~/.vim/colors
+
+mv solarized.vim ~/.vim/colors/
+
+mv vimrc ~/.vimrc
